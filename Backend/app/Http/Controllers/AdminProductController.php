c124b05e6266d2905ea2de7975e4b71d7fab4da8
Sophea Reaksa <Sopheareaksapheak@gmail.com>
2026-08-24 01:04:56 +0700

    Sync Admin instant product update features across repos

--- Backend/app/Http/Controllers/AdminProductController.php
+++ Backend/app/Http/Controllers/AdminProductController.php
@@ -15,16 +15,27 @@ class AdminProductController extends Controller
     public function store(Request $request)
     {
         $data = $request->validate([
-            'product_name' => 'required|string',
-            'product_category' => 'required|string',
+            'product_name' => 'required|string|max:255',
+            'product_category' => 'required|string|max:100',
             'product_price' => 'required|numeric',
             'product_discount' => 'nullable|numeric',
             'product_special_offer' => 'nullable|integer',
-            'product_image' => 'nullable|string',
-            'product_image2' => 'nullable|string',
-            'product_image3' => 'nullable|string',
-            'product_image4' => 'nullable|string',
+            'product_image' => 'nullable',
+            'product_image2' => 'nullable',
+            'product_image3' => 'nullable',
+            'product_image4' => 'nullable',
         ]);
+
+        foreach (['product_image', 'product_image2', 'product_image3', 'product_image4'] as $imgKey) {
+            if ($request->hasFile($imgKey)) {
+                $file = $request->file($imgKey);
+                $filename = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
+                $file->move(public_path('img'), $filename);
+                $data[$imgKey] = $filename;
+            } elseif (!$request->filled($imgKey)) {
+                $data[$imgKey] = null;
+            }
+        }
 
         $data['created_at'] = now();
         $product = Product::create($data);
@@ -34,20 +45,32 @@ class AdminProductController extends Controller
     public function update(Request $request, $id)
     {
         $product = Product::findOrFail($id);
+
         $data = $request->validate([
-            'product_name' => 'sometimes|string',
-            'product_category' => 'sometimes|string',
+            'product_name' => 'sometimes|string|max:255',
+            'product_category' => 'sometimes|string|max:100',
             'product_price' => 'sometimes|numeric',
-            'product_discount' => 'sometimes|numeric',
-            'product_special_offer' => 'sometimes|integer',
-            'product_image' => 'sometimes|string',
-            'product_image2' => 'sometimes|string',
-            'product_image3' => 'sometimes|string',
-            'product_image4' => 'sometimes|string',
+            'product_discount' => 'nullable|numeric',
+            'product_special_offer' => 'nullable|integer',
+            'product_image' => 'nullable',
+            'product_image2' => 'nullable',
+            'product_image3' => 'nullable',
+            'product_image4' => 'nullable',
         ]);
 
+        foreach (['product_image', 'product_image2', 'product_image3', 'product_image4'] as $imgKey) {
+            if ($request->hasFile($imgKey)) {
+                $file = $request->file($imgKey);
+                $filename = time() . '_' . uniqid() . '.' . $file->getClientOriginalExtension();
+                $file->move(public_path('img'), $filename);
+                $data[$imgKey] = $filename;
+            } elseif ($request->has($imgKey) && is_string($request->input($imgKey))) {
+                $data[$imgKey] = $request->input($imgKey);
+            }
+        }
+
         $product->update($data);
-        return response()->json($product);
+        return response()->json($product->fresh());
     }
 
     public function destroy($id)
